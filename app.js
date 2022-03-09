1dbc4973a5e5984a0248bee9f7ad992b114e27fd
s-lasch <91107394+s-lasch@users.noreply.github.com>
2022-03-08 21:17:04 -0500

    undo previous commit

--- app.js
+++ app.js
@@ -4,21 +4,7 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.status(200).json({
-        status: "Success",
-        message: "Welcome to project1!"
-    })
-})
-
 app.use(express.static('public'));
-
-app.use((req, res, next) => {
-    console.log("Hello from the middleware!");
-    next();
-})
-
-
 
 app.listen(port, () => {
     console.log(`App is listeing on port ${port}...`);
